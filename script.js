01985cc06661f0f8c8d8542e0574c414f8cf5d70
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-10-21 10:11:08 -0300

    21/10

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave(){
-    alert("Fui clicado!");
+    const texto = document.querySelector("#entrada-de-texto").ariaValueMax;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    cont palavras = texto.split(" ");
+
+    campoResultado = palavras.join(", ")
 }
