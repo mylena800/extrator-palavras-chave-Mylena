96fcf768715c1f11738c104abdeffaeb5059b919
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-10-28 10:14:48 -0300

    28/10

--- script.js
+++ script.js
@@ -13,5 +13,17 @@ function mostraPalavrasChave(){
 function procesaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
+    let frequencias = {};
+
+    for (let i of palavras){
+        frequencias[i]=0;
+
+        for (let j of palavras){
+            if(i == j){
+                frequencias[i]++;
+            }
+        }
+    }
+
     return palavras;
 }
